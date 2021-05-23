fc98b7c189fd1adc8ad15dd663f82083417d57e9
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2021-05-23 13:51:16 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,39 @@
-var firstName ="Kristian";
-var nameContainer = document.querySelector("#name")
+//Question 1
+
+var $num = 7;
+var $boo = false;
+var $str ="Very nice";
+
+//console.log($num)
+
+//Question 2
+
+var name = "Kristian";
+var lastName ="Koloy";
+var fullName = (name+lastName)
+
+//console.log(fullName)
+
+//Question 3
+
+var $animal ="frog";
+var $string ="The type of frog is"
+var typeOfAnimal = typeof $animal;
+
+//console.log($string+typeOfAnimal)
+
+//Question 4
+
+var orderHasShipped = true;
+if (orderHasShipped === true) {
+    //console.log("The order shipped");
+}
+else{
+    //console.log("The order did not ship");
+}
+
+//Question 5
+
+for (var i = 7; i <=13; i++){
+    //console.log(i);
+}
