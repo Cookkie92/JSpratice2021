5005ba84383f1fd493faa15bad449c352e1e9631
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2021-08-17 16:40:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,22 +5,22 @@
 
 var $num = 7;
 var $boo = false;
-var $str ="Very nice";
+var $str = "Very nice";
 
 //console.log($num)
 
 //Question 2
 
 var name = "Kristian";
-var lastName ="Koloy";
+var lastName = "Koloy";
 var fullName = (name + lastName)
 
 //console.log(fullName)
 
 //Question 3
 
-var $animal ="frog";
-var $string ="The type of frog is"
+var $animal = "frog";
+var $string = "The type of frog is"
 var typeOfAnimal = typeof $animal;
 
 //console.log($string+typeOfAnimal)
@@ -31,13 +31,13 @@ var orderHasShipped = true;
 if (orderHasShipped === true) {
     //console.log("The order shipped");
 }
-else{
+else {
     //console.log("The order did not ship");
 }
 
 //Question 5
 
-for (var i = 7; i <=13; i++){
+for (var i = 7; i <= 13; i++) {
     //console.log(i);
 }
 
@@ -67,17 +67,17 @@ var pets = [
         age: 5.5,
     },
     {
-        type:"dog",
+        type: "dog",
         age: 3.8,
     },
     {
-        type:"parrot",
-        age:4.0,
+        type: "parrot",
+        age: 4.0,
     },
 ];
 
-for(var i = 0; i < pets.length; i++){
-    if(pets[i].age >= 4) {
+for (var i = 0; i < pets.length; i++) {
+    if (pets[i].age >= 4) {
         //console.log(pets[i].type)
     }
 }
@@ -90,19 +90,19 @@ for(var i = 0; i < pets.length; i++){
 
 //declare function
 function boolValue(argue) {
-    if(argue === true || argue === false) {
-        //console.log(argue)
+    if (argue === true || argue === false) {
+        console.log(argue)
     }
-    //else if(argue === false){
-        //console.log(argue)
-    //}
-     else{
-        //console.log("Please pass a boolean value in")
+    else if (argue === false) {
+        console.log(argue)
+    }
+    else {
+        console.log("Please pass a boolean value in")
     }
 
 }
 //call function
-//boolValue();
+boolValue(boolValue);
 
 
 
@@ -115,7 +115,7 @@ function boolValue(argue) {
 var headline = document.querySelector("h2");
 var button = document.querySelector("button");
 
-function changeHeadline (){
+function changeHeadline() {
     headline.innerHTML = "Updated Subheading";
     headline.style.color = "blue";
 }
@@ -132,10 +132,10 @@ button.onclick = changeHeadline;
 
 var outOfStock = true;
 
-if(outOfStock === true){
+if (outOfStock === true) {
     //console.log("Out of stock");
 }
-else{
+else {
     //console.log("In stock");
 }
 
@@ -143,9 +143,9 @@ else{
 //Question 2
 
 
-for (var i = 15; i <=25; i++){
-    if(i === 17 || i === 20 ){ 
-        console.log(i);   
+for (var i = 15; i <= 25; i++) {
+    if (i === 17 || i === 20) {
+        console.log(i);
     }
 }
 
@@ -157,26 +157,26 @@ for (var i = 15; i <=25; i++){
 //Question 3
 
 var games = [
-	{
-		title: "Grand Theft Auto",
-		rating: 4.48,
-	},
-	{
-		title: "Portal 2",
-		rating: 3.5,
-	},
-	{
-		title: "Team Fortress",
-		rating: null,
-	},
-	{
-		title: "The Witcher",
-		rating: 3.0,
-	},
-	{
-		title: "The Elder Scrolls",
-		rating: 2.9,
-	},
+    {
+        title: "Grand Theft Auto",
+        rating: 4.48,
+    },
+    {
+        title: "Portal 2",
+        rating: 3.5,
+    },
+    {
+        title: "Team Fortress",
+        rating: null,
+    },
+    {
+        title: "The Witcher",
+        rating: 3.0,
+    },
+    {
+        title: "The Elder Scrolls",
+        rating: 2.9,
+    },
 ];
 
 
